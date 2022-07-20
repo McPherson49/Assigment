9c7a580caa01448c73228232a87a4a4285667942
McPherson49 <afolabipeter49@gmail.com>
2022-07-20 14:17:27 +0100

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-// question 1
+// Write a JavaScript function that reverse a number.
+// Example x = 32243;
+// Expected Output : 34223
 let goodness = [32243] ;
 let aji=goodness.reverse();
 let efe = aji.join();
 console.log (aji);
 
-// question 2
+// Write a JavaScript function that checks whether a passed string is palindrome or not?
 function dome (input) {
   if ( typeof(input) === 'string') {
     input = input.toLowerCase().replace (/[^A-Za-z0–9]/g)
@@ -19,7 +21,8 @@ console.log(dome('nurses run'));
 console.log(dome('madam')); 
 
 
-// question 3
+// Write a JavaScript function that accepts a string as a parameter and converts the first letter of each word of the string in upper case.
+
 var peter = prompt(" Enter your word ");
 var arr = peter.split(" ");
 for (var i=0; i< arr.length; i++) {
@@ -29,7 +32,8 @@ var peter2 = arr.join (" ");
 console.log(peter2)
 
 
-// question 5 
+// Write a JavaScript program to find the most frequent item of an array.
+ 
 var arr1 = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 var mf = 1;
 var m = 0;
@@ -49,7 +53,10 @@ for (var i = 0; i < arr1.length; i++) {
 
 alert(item + " ( " + mf + " times ) ");
 
-// question 6
+// Write a JavaScript conditional statement to find the sign of product of three numbers. Display an alert box with the specified sign.
+// Sample numbers : 3, -7, 2
+// Output : The sign is -
+
 var x=3;
 var y=-7;
 var z=2;
@@ -74,7 +81,7 @@ else if (x<0 && y<0 && z<0)
           console.log("The sign is -");
         }
         
-// question 7
+// Write a JavaScript conditional statement to find the largest of five numbers. Display an alert box to show the result.
 let a= -5
 b = -2
 c = -6
@@ -95,6 +102,29 @@ if (d>b && d>c && d>a && d>e) {
 if (e>b && e>c && e>d && e>a) {
     alert (e)
 }
+
+
+// How to write a
+function square(a,b,c){
+  let x = (-b + Math.sqrt(b**2 - 4*a*c))/2*a;
+  let y = (-b - Math.sqrt(b**2 - 4*a*c))/2*a;
+  console.log (x,y);
+}
+square(1,-6,-16);
+
+// Write a function that calculate the equation of a line and the result is in form of a strings
+function line(x1,x2,y1,y2) {
+  let m = (y2-y1)/(x2-x1);
+  var c = -(m*x1)+y1;
+  // let y = m + c;
+  console.log(`y=${m}x + ${c}`);
+}
+line (1,-2,3,4);
+
+
+
+
+
 
 
 
